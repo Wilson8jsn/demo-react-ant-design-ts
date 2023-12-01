f740d61fbe558af4e27457b50a2d03e36d64c524
Wilson Castillo <dc105677543@gmail.com>
2023-12-01 08:44:34 -0500

    commiy

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MoviePage from './components/MoviePage';
 
 const movies = [
   { title: 'Película 1', videoUrl: 'https://res.cloudinary.com/dcba9lt2s/video/upload/v1701026652/samples/sea-turtle.mp4' },
-  { title: 'Película 2', videoUrl: 'URL_DE_TU_PELICULA_2.mp4' },
+  { title: 'Película 2', videoUrl: 'https://res.cloudinary.com/dcba9lt2s/video/upload/v1701435106/hbi4n0jgf7fqyl0jw8dw.mp4' },
   { title: 'Película 3', videoUrl: 'URL_DE_TU_PELICULA_3.mp4' },
 
 ];
