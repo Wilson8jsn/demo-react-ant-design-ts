ec5fa367e2ecccfa49d66048050daf18d84a6c38
Wilson Castillo <dc105677543@gmail.com>
2023-12-01 00:29:21 -0500

    commit

--- src/components/MoviePage.tsx
+++ src/components/MoviePage.tsx
@@ -1,9 +1,8 @@
 // En src/components/MoviePage.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Card } from 'antd';
-
-
-
+import classNames from 'classnames';
+import './MoviePage.css'; // Asegúrate de tener un archivo CSS para tus estilos
 
 interface Movie {
   title: string;
@@ -13,18 +12,42 @@ interface Movie {
 interface MoviePageProps {
   movies: Movie[];
 }
+
 const MoviePage: React.FC<MoviePageProps> = ({ movies }) => {
+  const [currentVideo, setCurrentVideo] = useState<string | null>(null);
+
+  const playVideo = (videoUrl: string) => {
+    setCurrentVideo(videoUrl);
+  };
+
   return (
-    <div style={{ padding: '20px' }}>
-      <h1 style={{ marginBottom: '20px' }}>Películas</h1>
-      {movies.map((movie, index) => (
-        <Card key={index} style={{ marginBottom: '20px' }}>
-          <h2>{movie.title}</h2>
-          <Button type="primary" onClick={() => window.open(movie.videoUrl, '_blank')}>
-            Reproducir
+    <div className="movie-page">
+      <h1 className="movie-page-title">Películas</h1>
+      <div className="movie-list">
+        {movies.map((movie, index) => (
+          <Card
+            key={index}
+            className={classNames('movie-card', { 'last-card': index === movies.length - 1 })}
+          >
+            <h2 className="movie-title">{movie.title}</h2>
+            <Button
+              type="primary"
+              className="play-button"
+              onClick={() => playVideo(movie.videoUrl)}
+            >
+              Reproducir
+            </Button>
+          </Card>
+        ))}
+      </div>
+      {currentVideo && (
+        <div className="video-container">
+          <video controls autoPlay src={currentVideo} className="current-video" />
+          <Button className="back-button" onClick={() => setCurrentVideo(null)}>
+            Volver
           </Button>
-        </Card>
-      ))}
+        </div>
+      )}
     </div>
   );
 };
