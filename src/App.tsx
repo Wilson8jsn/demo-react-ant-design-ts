7c172efe5ed2955930a1cf7ace761dc783c81c3a
Wilson Castillo <115800617+Wilson8jsn@users.noreply.github.com>
2023-12-22 16:06:43 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import { Card } from 'antd';
 
-const { Meta } = Card;
+import React from 'react';
+import AppCard from './components/card'; 
 
 const App: React.FC = () => (
-  <Card
-    hoverable
-    style={{ width: 240, marginTop: 200, justifyContent: 'center', marginLeft: '600px' }}
-    cover={<img alt="example" src="https://res.cloudinary.com/dcba9lt2s/image/upload/v1702476736/onbqxbd3sf9rmrrgvjkt.webp" />}
-  >
-    <Meta
-      title={
-        <div>
-          <span style={{ color: '#FF00E8' }}>REACT GRAHQL DEVELOP</span>
-          <br />
-          <span style={{ color: '#FF00E8' }}>FOR ANY DEVELOPMENT</span>
-        </div>
-      }
-      description="Best one bars to take friends and relatives, one bars to take friends and relatives"
-    />
-  </Card>
+  <div>
+    <AppCard />
+  </div>
 );
 
 export default App;
