896e56b228f16ec3ee408a1607b905f11c801a35
Wilson Castillo <115800617+Wilson8jsn@users.noreply.github.com>
2023-12-22 16:12:29 -0500

    commit

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,7 +7,7 @@ const AppCard: React.FC = () => (
   <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
     <Card
       hoverable
-      style={{ width: 240 }}
+      style={{ width: 240, flex: 1 }}
       cover={<img alt="example" src="https://res.cloudinary.com/dcba9lt2s/image/upload/v1702476736/onbqxbd3sf9rmrrgvjkt.webp" />}
     >
       <Meta
